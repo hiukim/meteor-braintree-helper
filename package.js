050dce8e7c4a723d1f2e6c84dd6247fdf0fa51cb
hiukim <hiukim@gmail.com>
2015-08-07 12:17:23 +0800

    update braintree client script

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'hiukim:braintree-helper',
-  version: '1.0.0',
+  version: '1.0.1',
   // Brief, one-line summary of the package.
   summary: 'Just a braintree helper',
   // URL to the Git repository containing the source code for this package.
